1ad3d4dce169a58879bd79dd7979fc9bce380960
Biraj Rai <valxud1@gmail.com>
2023-03-05 01:11:11 +0545

    feat: [+] added config

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    token: 'MTA1MzYxMzQ4NTY4NDE3NDkxOQ.GRLOgo.nOoSs26s6nAQeEr4jLPLVejTGrJEiiuIAmqs9Q',
-    client_id: '1053613485684174919',
-    guild_id: '1031456718040870912',
-    developer: '835126233455919164',
-    username: 'qwertynepal',
-    password: 'qwertynepal',
-    server: 'pikameow',
+    token: process.env['token'],
+    client_id: '',
+    guild_id: '',
+    // developer: '', Developer User ID
+    username: process.env['username'],
+    password: process.env['password'],
+    server: 'example', //Server Name
 };
